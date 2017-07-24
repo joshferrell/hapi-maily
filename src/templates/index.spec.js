51d847f8a7a5642cb7059e26f2dbc87015c6998e
Joshua Ferrell <josh@joshferrell.me>
2017-07-24 14:23:58 -0700

    chore: updated snapshot tests to work

--- src/templates/index.spec.js
+++ src/templates/index.spec.js
@@ -4,4 +4,8 @@ import initStoryshots from '@storybook/addon-storyshots';
 
 /* eslint-enable import/no-extraneous-dependencies, import/first */
 
+jest.mock('react-dom', () => ({
+    findDOMNode: () => ({})
+}));
+
 initStoryshots();
