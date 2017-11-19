eec7036a6a0083c236a4e5c1a28a922ecf77698b
Josh Ferrell <jferrell@joshs-mbp.attlocal.net>
2017-11-18 19:47:15 -0800

    feat: removed server render template and used widget function

--- server/src/connection/email/render-template.js
+++ server/src/connection/email/render-template.js
@@ -1,43 +1,10 @@
-import React from 'react';
 import { renderToStaticMarkup } from 'react-dom/server';
-import { mjml2html } from 'mjml';
 import { minify } from 'html-minifier';
+import { renderMJML } from 'hapi-maily-widgets';
 import { pipe } from 'ramda';
-
-export const renderComponent = component => ({
-    __html: mjml2html(`
-        <mjml>
-            <mj-head>
-             <mj-style>
-                 .display-none {
-                     display: none !important;
-                 }
-             </mj-style>
-             <mj-attributes>
-               <mj-all font-family="Helvetica, Arial, sans-serif" />
-             </mj-attributes>
-           </mj-head>
-            <mj-body>
-                <mj-container>
-                    ${renderToStaticMarkup(component)}
-                </mj-container>
-            </mj-body>
-        </mjml>
-    `).html
-});
-
-// mjml requires that we set the inner html dangerously
-// this should be fine so long as you're not doing
-// anything strange with creating these templates, just be aware.
-export const renderMJML = component => (
-    <div
-        dangerouslySetInnerHTML={renderComponent(component)} // eslint-disable-line react/no-danger
-    />
-);
 
 const renderTemplate = pipe(
     renderMJML,
-    renderComponent,
     renderToStaticMarkup,
     minify
 );
